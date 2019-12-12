4c3beee6af319bd349e56c3de2e5ef0f8127a58c
Sam Vargas <sam.vargas@gmx.com>
2019-12-12 18:33:08 +0000

    books model and api working

--- starter-code/routes/api/book.js
+++ starter-code/routes/api/book.js
@@ -21,8 +21,22 @@ router.post("/", (req, res, next) => {
     image
   } = req.body;
   // const body = req.body;
-
-  res.json({ body });
+  //Create new book
+  Book.create({
+    title,
+    author,
+    synopsis,
+    type,
+    genre,
+    language,
+    pushlished_year,
+    price,
+    image
+  }, (err, book) => {
+    if (err) return console.log(err);
+  });
+  // res.json({ title, author, synopsis, type, genre, language, pushlished_year, price, image });
+  res.json({ "message": "OK" });
 });
 
 module.exports = router;
