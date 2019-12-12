f2f670e55e09e40694165dbe4960d590129ce585
Marco <manhani@gmail.com>
2019-12-12 18:37:02 +0000

    test commit

--- starter-code/routes/api/index.js
+++ starter-code/routes/api/index.js
@@ -2,14 +2,17 @@
 
 const { Router } = require("express");
 const router = new Router();
-const routeGuard = require("../../middleware/route-guard");
+const routeGuard = require("
+sdf
+../../middleware/route-guard");
 
 router.get("/", (req, res, next) => {
   res.json({ type: "success", data: { title: "Hello World" } });
 });
-
+dasdjsadas
 router.get("/private", routeGuard, (req, res, next) => {
   res.render("private");
+  asfasfda
 });
 
 module.exports = router;
