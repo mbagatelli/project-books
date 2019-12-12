f5e3d129edbc5de4a0cc868c0df774eedb6c160e
Sam Vargas <sam.vargas@gmx.com>
2019-12-12 18:41:03 +0000

    it's nice

--- starter-code/routes/api/index.js
+++ starter-code/routes/api/index.js
@@ -2,19 +2,14 @@
 
 const { Router } = require("express");
 const router = new Router();
-const routeGuard = require("
-sdf
-../../middleware/route-guard");
+const routeGuard = require("../../middleware/route-guard");
 
 router.get("/", (req, res, next) => {
   res.json({ type: "success", data: { title: "Hello World" } });
 });
-dasdjsadas
+
 router.get("/private", routeGuard, (req, res, next) => {
   res.render("private");
-  asfasfda
-});
+  });
 
 module.exports = router;
-
-//this is a comment
