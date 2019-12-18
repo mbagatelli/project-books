94f435ac5e43c0931db356c3ace398789e27ccfe
Sam Vargas <sam.vargas@gmx.com>
2019-12-18 20:39:24 +0000

    fixed merge conflict

--- starter-code/client/src/views/BuyView.jsx
+++ starter-code/client/src/views/BuyView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, Form } from "react-bootstrap";
 
