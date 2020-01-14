3471dd9e7000274b1674c29b9ec5a6628f64431d
Sam Vargas <sam.vargas@gmx.com>
2020-01-14 10:35:20 +0000

    fixed copy on homepage

--- starter-code/client/src/views/HomeView.jsx
+++ starter-code/client/src/views/HomeView.jsx
@@ -9,35 +9,49 @@ class HomeView extends Component {
 
   render() {
     return (
-      <div className='home'>
-        <div className='hero'>
+      <div className="home">
+        <div className="hero">
           <h1>Sharing a world of possibilities through books</h1>
         </div>
-        <div className='mission'>
-          <div className='home-content'>
-            <h2 className='m-5'>Books are expensive.</h2>
+        <div className="mission">
+          <div className="home-content">
+            <h2 className="m-5">Books are expensive.</h2>
             <p>
               For many people books are just too expensive and others have books
               collecting dust on shelves.
             </p>
-            <p className='mt-5'>What if we could change that?</p>
+            <p className="mt-5">What if we could change that?</p>
           </div>
         </div>
-        <div className='howitworks mt-0'>
-          <div className='home-content mt-0 pt-0'>
-            <h2 className='mr-3 ml-3 mb-5'>How it works</h2>
+        <div className="howitworks mt-0">
+          <div className="home-content mt-0 pt-0">
+            <h2 className="mr-3 ml-3 mb-5 mt-0">How it works</h2>
+            <p className="m-0">
+              List a book<br />
+              Send a book<br />
+              Receive coins<br />
+              Spend coins
+            </p>
+            <p>Magic</p>
             <p>
-              <Link to='/user/sign-up'>Sign up</Link> to the site and
-              <Link to='/book/sell'> sell</Link> your first book!
+              <Link to="/user/sign-up">Sign up</Link> to the site and
+              <Link to="/book/sell"> sell</Link> your first book!
             </p>
-            <p className="m-3">
-              Now sure what to read?<br />
-              Check out <a href="https://mind-scape.herokuapp.com/">MindScape</a>
+            <p>
+              Haven't got a book to trade? We've got you covered: You can buy
+              coins. Sorted.
             </p>
-            <Link to='/book/buylist' type="button" class="btn btn-primary mt-3" >See Books</Link>
+            <p>
+              Not sure what to read?
+              <br />
+              Check out{" "}
+              <a href="https://mind-scape.herokuapp.com/">MindScape</a>
+            </p>
+            <Link to="/book/buylist" type="button" class="btn btn-primary mt-3">
+              See Books
+            </Link>
           </div>
         </div>
-
       </div>
     );
   }
